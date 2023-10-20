bf69f751223afef603a21ca3c4916e21afdcc925
Mohammad Afzal <afzalex.store@gmail.com>
2023-10-20 18:00:01 +0530

    Fixed event listener

--- src/main/java/com/fz/Main.java
+++ src/main/java/com/fz/Main.java
@@ -18,13 +18,28 @@ public class Main {
     public static final LocalTime TIMEFRAME_START = LocalTime.of(9, 0);
     public static final LocalTime TIMEFRAME_END = LocalTime.of(22, 0);
 
+
+    private final Reference<LocalTime> lastActivityTime = new Reference<>(LocalTime.now());
+    private final GlobalMouseMovementListener mouseMovementListener = new GlobalMouseMovementListener(lastActivityTime);
+
+    private void activateKeyDetector() {
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListener(lastActivityTime));
+    }
+
+    private void activateMouseDetector() {
+        GlobalScreen.addNativeMouseListener(mouseMovementListener);
+        GlobalScreen.addNativeMouseMotionListener(mouseMovementListener);
+    }
+
+    private void deActivateMouseDetector() {
+        GlobalScreen.removeNativeMouseListener(mouseMovementListener);
+        GlobalScreen.removeNativeMouseMotionListener(mouseMovementListener);
+    }
+
     public void run() throws NativeHookException, AWTException {
         log.info("Welcome to TapAlive");
-        Reference<LocalTime> lastActivityTime = new Reference<>(LocalTime.now());
         GlobalScreen.registerNativeHook();
-        GlobalScreen.addNativeKeyListener(new GlobalKeyListener(lastActivityTime));
-        GlobalScreen.addNativeMouseListener(new GlobalMouseMovementListener(lastActivityTime));
-        GlobalScreen.addNativeMouseMotionListener(new GlobalMouseMovementListener(lastActivityTime));
+
         Robot bot = new Robot();
         while (true) {
             var now = LocalTime.now();
@@ -37,11 +52,13 @@ public class Main {
                 log.info("Ignoring because recent activity detected at {}", lastActivityTime.get());
                 continue;
             }
+            deActivateMouseDetector();
             Point pointerLocation = MouseInfo.getPointerInfo().getLocation();
             log.info("Triggered mouse down at {}", pointerLocation);
             bot.mouseMove(pointerLocation.x, pointerLocation.y);
             bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            activateMouseDetector();
         }
     }
 
